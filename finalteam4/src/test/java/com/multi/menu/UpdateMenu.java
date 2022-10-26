fec837b0edf6b2395f354ce6c65ac76773874cd8
the-escort <vanitas613@naver.com>
2022-10-26 18:18:44 +0900

    update menu, review update test

---
+++ finalteam4/src/test/java/com/multi/menu/UpdateMenu.java
@@ -0,0 +1,25 @@
+package com.multi.menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.multi.dto.MenuDTO;
+import com.multi.service.MenuService;
+
+@SpringBootTest
+public class UpdateMenu {
+
+	@Autowired
+	MenuService menu_service;
+
+	@Test
+	void contextLoads() {
+		try {
+			menu_service.modify(new MenuDTO(2, 1, 2, "한상차림", 20000, "한상차림", "2.img"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Update Ok");
+	}
+}
---
+++ finalteam4/src/test/java/com/multi/review/UpdateReview.java
@@ -0,0 +1,25 @@
+package com.multi.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.multi.dto.ReviewDTO;
+import com.multi.service.ReviewService;
+
+@SpringBootTest
+public class UpdateReview {
+
+	@Autowired
+	ReviewService review_service;
+
+	@Test
+	void contextLoads() {
+		try {
+			review_service.modify(new ReviewDTO(6, "id07", 2, "별로네요...", 1));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Update Ok");
+	}
+}
