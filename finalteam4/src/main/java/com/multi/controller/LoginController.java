451bf9881d596a285bca4a2ec9fbc9d3f892047a
junki-stevekim <junkistevekim@gmail.com>
2022-10-27 14:40:56 +0900

    Login -Junki

---
+++ finalteam4/src/main/java/com/multi/controller/LoginController.java
@@ -0,0 +1,41 @@
+package com.multi.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.multi.dto.CustDTO;
+import com.multi.service.CustService;
+
+@Controller
+public class LoginController {
+
+	@Autowired
+	CustService service;
+	
+	@RequestMapping("/loginimpl")
+	public String loginimpl(HttpSession session, String custid, String custpwd, Model model) {
+		CustDTO cust = null;
+		System.out.println("in");
+		try {
+			cust = service.get(custid);
+			if(cust != null) {
+				if(cust.getCustpwd().equals(custpwd)) {	
+				session.setAttribute("logincust", cust);
+				model.addAttribute("center", "maincenter");
+				}else {
+					model.addAttribute("center", "loginfail");
+				}
+			}
+		} catch (Exception e) {
+			
+		}
+		return "index";
+	}
+
+	
+	
+}
