58a99af77da20a387f7f47bda2d237dec26a2be9
YongHunKo <dydgnsrh5460@naver.com>
2022-11-03 15:54:17 +0900

    add CartCRUD_221103

---
+++ finalteam4/src/main/java/com/multi/dto/CartDTO.java
@@ -0,0 +1,20 @@
+package com.multi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class CartDTO {
+	private Integer cartid;
+	private String custid;
+	private Integer menuid;
+	private int cnt;
+
+}
