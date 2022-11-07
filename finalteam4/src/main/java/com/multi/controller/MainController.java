5ad338f3906d21bf8f1b4fb43a2765933a5be4d3
syp00 <syp0530@gmail.com>
2022-11-07 09:11:16 +0900

    maincenter controller// sypark

--- finalteam4/src/main/java/com/multi/controller/MainController.java
+++ finalteam4/src/main/java/com/multi/controller/MainController.java
@@ -1,16 +1,35 @@
 package com.multi.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.multi.dto.WishlistDTO;
+import com.multi.service.WishlistService;
+
 @Controller
 public class MainController {
+	
+	@Autowired
+	WishlistService wishservice;
 
 	@RequestMapping("/")
-	public String main() {
+	public String main(String custid, Model model) {
+		List<WishlistDTO> list = null;
+		try {
+			list = wishservice.viewwish(custid);
+			model.addAttribute("viewwish", list);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
 		return "index";
 	}
+
 
 
 	@RequestMapping("/login")
