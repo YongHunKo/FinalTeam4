37b1f979685f5022474df7b9001c6b851252c9fe
YongHunKo <dydgnsrh5460@naver.com>
2022-11-09 15:19:49 +0900

    connect categorydetail between storedeatil_221109

---
+++ finalteam4/src/test/java/com/multi/store/DetailimgStore.java
@@ -0,0 +1,31 @@
+package com.multi.store;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.multi.dto.StoreDTO;
+import com.multi.service.StoreService;
+
+@SpringBootTest
+class DetailimgStore {
+	@Autowired
+	StoreService service;
+
+	@Test
+	void contextLoads() {
+		List<StoreDTO> list = null;
+		try {
+			list = service.detailimg(200);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		for(StoreDTO s:list) {
+			System.out.println(s);
+		}
+	}
+
+}
