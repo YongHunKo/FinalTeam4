a88c9514d6bac6c35176adb49295cd635645f386
junki-stevekim <junkistevekim@gmail.com>
2022-11-10 17:42:46 +0900

    메인화면 img -> cover fix,loginfail fix -Junki

--- finalteam4/src/main/java/com/multi/controller/KaKaoController.java
+++ finalteam4/src/main/java/com/multi/controller/KaKaoController.java
@@ -32,13 +32,14 @@ public class KaKaoController {
 	
 	 /**
 	  * kakaologin
-	  * 해당 메소드는 accessToken을 통해 카카오로 유저DB를 받아오고
+	  * 해당 메소드는  oauth2.0을 기반으로한
+	  * accessToken을 통해 카카오서버에서 인증허가토큰을통해 유저DB를 받아오고
 	  * HashMap으로 키값은 String, value값은 JSONObject로 userInfo에 담아서
 	  * userInfo에 담긴 값들을 추출하여 
 	  * cust에 해당 카카오로그인 데이터가 없을 시 register,
 	  * cust에 해당 카카오로그인 데이터가 있을 시 일반적인 로그인방식으로
 	  * 쉽게 로그인하는 것이 목적이다.
-	  * 부가적으로 랜덤이미지,랜덤정보를 출력한다.
+	  * 부가적으로 해당유저의 프로필이미지,유저네임을 출력할수있도록 값을 추출한다.
 	  * @param code
 	  * @param session
 	  * @param model
@@ -53,10 +54,19 @@ public class KaKaoController {
 	    /*랜덤이미지, 랜덤정보 부분*/
 	    List<StoreimgDTO> list2=null;
 	    List<StoreimgDTO> list3=null;
+		List<StoreimgDTO> list4=null;
+		List<StoreimgDTO> list5=null;
+		List<StoreimgDTO> list6=null;
 	    list2 = imgservice.selectrandom();
 	    list3 = imgservice.selectrandominfo();
+	    list4 = imgservice.today();
+		list5 = imgservice.today2();
+		list6 = imgservice.today3();
 	    model.addAttribute("randomimg", list2);
 	    model.addAttribute("randominfo", list3);
+	    model.addAttribute("today", list4);
+		model.addAttribute("todaytwo", list5);
+		model.addAttribute("todaythree", list6);
 	    
 	    /*소셜로그인 데이터를 userInfo에 담아서 추출하는 부분*/
 	    model.addAttribute("userId", userInfo);
--- finalteam4/src/main/java/com/multi/controller/NaverController.java
+++ finalteam4/src/main/java/com/multi/controller/NaverController.java
@@ -37,10 +37,21 @@ public class NaverController {
        System.out.println("login Controller : " + userInfo);
        	List<StoreimgDTO> list2=null;
 		List<StoreimgDTO> list3=null;
+		List<StoreimgDTO> list4=null;
+		List<StoreimgDTO> list5=null;
+		List<StoreimgDTO> list6=null;
 		list2=imgservice.selectrandom();
 		list3=imgservice.selectrandominfo();
 		model.addAttribute("randomimg", list2);
 		model.addAttribute("randominfo", list3);
+		list4 = imgservice.today();
+		list5 = imgservice.today2();
+		list6 = imgservice.today3();
+		model.addAttribute("randomimg", list2);
+		model.addAttribute("randominfo", list3);
+		model.addAttribute("today", list4);
+		model.addAttribute("todaytwo", list5);
+		model.addAttribute("todaythree", list6);
 		
 		model.addAttribute("naver", userInfo);
        model.addAttribute("center", "maincenter");
