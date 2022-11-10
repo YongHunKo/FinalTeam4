532ed3e91a5d1f269f5c357d1de7ef51fe11a054
YongHunKo <dydgnsrh5460@naver.com>
2022-11-10 10:21:11 +0900

    add comment ACtrl_221110

--- finalteam4/src/main/java/com/multi/controller/AController.java
+++ finalteam4/src/main/java/com/multi/controller/AController.java
@@ -38,6 +38,15 @@ public class AController {
 	@Autowired
 	ReserveService reserveservice;
 	
+	/**
+	 * checkid
+	 * 해당 메소드는 cid를 통해
+	 * cust에서 해당 cid와 맞는
+	 * custid를 검색하여 일치 여부를
+	 * result로 return해주기 위한 목적이다.
+	 * @param cid
+	 * @return
+	 */
 	@RequestMapping("/checkid")
 	public Object checkid(String cid) {
 		String result = "";
@@ -55,7 +64,17 @@ public class AController {
 		return result;
 	}
 	
-	//미완
+	/**
+	 * addcart
+	 * 해당 메소드는 storeid를 통해
+	 * store-menu-menuimgs로 묶인 관계에서
+	 * 검색된 데이터들을 cart에 register하는 것이 목적이다.
+	 * @param model
+	 * @param custid
+	 * @param cnt
+	 * @param storeid
+	 * @return
+	 */
 	@RequestMapping("/addcart")
 	public String addcart(Model model, String custid, Integer cnt, Integer storeid) {
 		List<StoreDTO> list = null;
@@ -70,6 +89,16 @@ public class AController {
 		return "";
 	}
 	
+	/**
+	 * reconfirm
+	 * 해당 메소드는 회원정보 수정에 관한 메소드이며
+	 * 파라메터로 받아온 custid와 custpwd를 통해
+	 * cust에서 해당 custid의 custpwd와 비교하여,
+	 * 일치 여부를 result로 리턴시키는 것이 목적이다.
+	 * @param custid
+	 * @param custpwd
+	 * @return
+	 */
 	@RequestMapping("/edit/reconfirm")
 	public Object reconfirm(String custid, String custpwd) {
 		String result = "";
