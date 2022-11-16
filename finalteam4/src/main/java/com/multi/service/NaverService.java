beebc2def437dd1bbaa5cdce896bfcac3bb18ee5
junki-stevekim <junkistevekim@gmail.com>
2022-11-16 17:32:43 +0900

    소셜로그인 Service ->서버용 redirect url 수정 -Junki

--- finalteam4/src/main/java/com/multi/service/NaverService.java
+++ finalteam4/src/main/java/com/multi/service/NaverService.java
@@ -40,6 +40,7 @@ public class NaverService {
             sb.append("&client_id=MAK4QIbORyQCqNxpf2Oc");
             sb.append("&client_secret=jgzzekxRIT");
             sb.append("&redirect_uri=http://127.0.0.1/naver/callback");
+			/* sb.append("&redirect_uri=http://118.67.131.90/naver/callback"); 서버전용 */
             sb.append("&code=" + authorize_code);
             sb.append("&state="+"test");
             bw.write(sb.toString());
