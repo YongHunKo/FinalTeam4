6acc545e22c4f32b42fa759dd3a03939171cac71
junki-stevekim <junkistevekim@gmail.com>
2022-11-18 11:29:23 +0900

    Main page, 소셜로그인 일시수정 ,detail -Junki

--- finalteam4/src/main/java/com/multi/service/KaKaoService.java
+++ finalteam4/src/main/java/com/multi/service/KaKaoService.java
@@ -38,6 +38,7 @@ public class KaKaoService {
 	            //    POST 요청을 위해 기본값이 false인 setDoOutput을 true로
 	            conn.setRequestMethod("POST");
 	            conn.setDoOutput(true);
+	          
 	            
 	            //    POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송
 	            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
