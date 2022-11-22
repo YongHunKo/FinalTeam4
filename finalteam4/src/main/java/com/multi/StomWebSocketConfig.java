8f2f1071aea8e1e53f5aa8efd35bf1cfa1f0c7ce
the-escort <vanitas613@naver.com>
2022-11-22 17:35:56 +0900

    update chatbotService, question.html - yeosong

--- finalteam4/src/main/java/com/multi/StomWebSocketConfig.java
+++ finalteam4/src/main/java/com/multi/StomWebSocketConfig.java
@@ -6,14 +6,14 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@EnableWebSocketMessageBroker
 @Configuration
+@EnableWebSocketMessageBroker
 public class StomWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/ws").setAllowedOrigins("http://127.0.0.1:8080","http://localhost:8080", "http://127.0.0.1").withSockJS();
-		//NCP를 이용할땐 포트가 1개로만 하니까 뒤에있는 주소 하나만 있으면될거같다
+		registry.addEndpoint("/ws").setAllowedOrigins("http://127.0.0.1:8080", "http://127.0.0.1").withSockJS();
+//		NCP를 이용할땐 포트가 1개로만 하니까 뒤에있는 주소 하나만 있으면될거같다
 		registry.addEndpoint("/wss").setAllowedOrigins("http://127.0.0.1:8080","http://localhost:8080", "http://127.0.0.1").withSockJS();
 		registry.addEndpoint("/chbot").setAllowedOrigins("http://127.0.0.1:8080","http://localhost:8080", "http://127.0.0.1").withSockJS();
 	}
@@ -21,6 +21,8 @@ public class StomWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	/* 어플리케이션 내부에서 사용할 path를 지정할 수 있음 */
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
-		registry.enableSimpleBroker("/send","/broadcast");
+//		registry.setApplicationDestinationPrefixes("/app");
+//		registry.enableSimpleBroker("/topic");
+		registry.enableSimpleBroker("/send","/broasdcast");
 	}
 }
--- finalteam4/src/main/java/com/multi/controller/MsgController.java
+++ finalteam4/src/main/java/com/multi/controller/MsgController.java
@@ -1,5 +1,6 @@
 package com.multi.controller;
 
+import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -8,30 +9,42 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import com.multi.dto.Msg;
-
-
+import com.multi.service.ChatbotService;
 
 @Controller
 public class MsgController {
-	
+
 	@Autowired
 	SimpMessagingTemplate template;
-	
-	@MessageMapping("/receiveall") // 모두에게 전송
-	public void receiveall(Msg msg, SimpMessageHeaderAccessor headerAccessor) {
-		System.out.println(msg);
-		template.convertAndSend("/send",msg);
-	}
-	@MessageMapping("/receiveme") // 나에게만 전송 ex)Chatbot
+
+	@Autowired
+	ChatbotService service;
+
+	@MessageMapping("/receiveme")
 	public void receiveme(Msg msg, SimpMessageHeaderAccessor headerAccessor) {
 		String id = msg.getSendid();
-		msg.setContent2("TR Message");
-		template.convertAndSend("/send/"+id,msg);
+		String answer = "";
+		try {
+			answer = service.sendMessage(msg.getContent1());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		msg.setContent2(answer);
+		System.out.println("answer : " + answer);
+		template.convertAndSend("/send/" + id, msg);
 	}
-	@MessageMapping("/receiveto") // 특정 Id에게 전송
-	public void receiveto(Msg msg, SimpMessageHeaderAccessor headerAccessor) {
-		String id = msg.getSendid();
-		String target = msg.getReceiveid();
-		template.convertAndSend("/send/to/"+target,msg);
-	}
+
+//	@MessageMapping("/receiveto") // 특정 Id에게 전송
+//	public void receiveto(Msg msg, SimpMessageHeaderAccessor headerAccessor) {
+//		String id = msg.getSendid();
+//		String answer = "";
+//		String target = msg.getReceiveid();
+//		try {
+//			answer = service.sendMessage(msg.getContent1());
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		msg.setContent2(answer);
+//		template.convertAndSend("/send/to/" + target, msg);
+//	}
 }
