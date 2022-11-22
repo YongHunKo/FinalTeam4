46157a696850c3c6ba2f931bdec1e5853547e09e
syp00 <syp0530@gmail.com>
2022-11-22 14:16:28 +0900

    admin chart sypark

--- finalteam4admin/src/main/java/com/multi/controller/AController.java
+++ finalteam4admin/src/main/java/com/multi/controller/AController.java
@@ -1,10 +1,16 @@
 package com.multi.controller;
 
+import java.util.List;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.multi.dto.ChartDTO;
 import com.multi.dto.EatAdmDTO;
+import com.multi.mapper.AMapper;
 import com.multi.service.EatAdmService;
 
 @RestController
@@ -12,6 +18,8 @@ public class AController {
 
 	@Autowired
 	EatAdmService admservice;
+	@Autowired
+	AMapper amapper;
 	
 	@RequestMapping("/checkid")
 	public Object checkid(String cid) {
@@ -33,6 +41,35 @@ public class AController {
 			e.printStackTrace();
 		}
 		
+		return result;
+	}
+	
+	@RequestMapping("/chart1")
+	public Object chart1() {
+		JSONObject result = new JSONObject();
+		
+		JSONArray ja = new JSONArray();
+		JSONObject obj = new JSONObject();
+		obj.put("name", "Customer");
+		
+		JSONArray uja = new JSONArray();
+		
+		List<ChartDTO> list = null;
+		
+		list = amapper.chart1();
+		
+		JSONArray day_ja = new JSONArray();
+		int d = 0;
+		for(ChartDTO c:list) {
+			uja.add(c.getTotal());
+			d++;
+			day_ja.add(d);	
+		}
+		obj.put("data", uja);
+		ja.add(obj);
+		
+		result.put("day",day_ja);
+		result.put("result", ja);
 		return result;
 	}
 }
