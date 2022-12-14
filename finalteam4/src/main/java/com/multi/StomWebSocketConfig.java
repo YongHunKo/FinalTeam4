25bff187325036109bf9c17ea857dc8717668aeb
YongHunKo <dydgnsrh5460@naver.com>
2022-12-14 13:33:24 +0900

    fix socketJS_221214

--- finalteam4/src/main/java/com/multi/StomWebSocketConfig.java
+++ finalteam4/src/main/java/com/multi/StomWebSocketConfig.java
@@ -12,7 +12,7 @@ public class StomWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/ws").setAllowedOrigins("http://127.0.0.1:8080", "http://127.0.0.1").withSockJS();
+		registry.addEndpoint("/ws").setAllowedOrigins("http://127.0.0.1:8080","http://118.67.131.90/eatnout","http://118.67.131.90/eatadmin",  "http://127.0.0.1").withSockJS();
 //		NCP를 이용할땐 포트가 1개로만 하니까 뒤에있는 주소 하나만 있으면될거같다
 		registry.addEndpoint("/wss").setAllowedOrigins("http://127.0.0.1:8080","http://localhost:8080", "http://127.0.0.1").withSockJS();
 		registry.addEndpoint("/chbot").setAllowedOrigins("http://127.0.0.1:8080","http://localhost:8080", "http://127.0.0.1").withSockJS();
